adcc9e8ebe99f9fbe27bd301b580903406efa217
JoeyLwq <linweiqiao@126.com>
2019-12-24 22:32:48 +0800

    update

--- my_common/src/main/java/utils/IdGenerator.java
+++ my_common/src/main/java/utils/IdGenerator.java
@@ -1,13 +1,15 @@
 package utils;
 
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 import java.util.Random;
 
 public class IdGenerator {
 
     public static String generatorId() {
-        Random random = new Random();
-        return String.valueOf(random.nextInt(9999999));
+        //生成6位随机数
+        return RandomStringUtils.randomNumeric(6);
     }
 }
 
